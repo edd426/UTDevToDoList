4ac42df3c97bdeebef598e68b7b31078184a264e
Evan DeLord <eddelord@gmail.com>
2017-10-02 16:25:43 -0500

    For now I ssimply have 5 numbered text boxes for the todo list. Next, I'll add a fake database so there's a dynamic number of things to do.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `
+        <h1>{{name}}:</h1>
+        <ul class="myList">
+            <li *ngFor="let thing of myList"
+                [class.selected]="thing === myThing"
+                (click)="onSelect(thing)">
+                <span class="badge">{{thing.id}}</span>
+                <input placeholder="Today I will...">
+            </li>
+        </ul>
+  
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+    name = 'TODO'; 
+    myList = THINGS;
+    myThing: Thing;
+
+}
+
+export class Thing {
+    id: number;
+}
+
+const THINGS: Thing[] = [
+    { id: 1 }
+    { id: 2 }
+    { id: 3 }
+    { id: 4 }
+    { id: 5 }
+];
